7d9ff2fb077d1db472f9cd193daf467439ee4c12
Teros <154768334+t-ros@users.noreply.github.com>
2025-02-24 12:26:08 +0000

    Update read_store.cpp

--- [03] Algorithmic_Sorting/Read and Store/read_store.cpp
+++ [03] Algorithmic_Sorting/Read and Store/read_store.cpp
@@ -7,49 +7,49 @@ using namespace std;
 
 int main()
 {
-     // Open the file and check
-    ifstream file(num_file.txt);
+    // Open the file and check
+    ifstream file("num_file.txt");
     if(!file){
-        cerr  Error opening the file!  endl;
+        cerr <<"Error opening the file!" <<  endl;
         return 1;
     }
-    cout  Opening the file  endl  ...  endl;
+    cout << "Opening file" << endl << "..." << endl;
 
     double num;
     int count = 0;
 
-    cout  a) The file contains the following data   endl;
+    cout << "a) The file contains the following data"  << endl;
 
-    while(file  num){  Read num while there is data
-        cout                      fixed  setprecision(3)  num  endl;
+    while(file >> num){ // Read num while there is data
+        cout << "                  "<<  fixed << setprecision(3) << num << endl;
         count++;
     }
 
-    cout  Total   count   numbers, rounded to 3 decimal places  endl;
+    cout << "Total: " << count << " numbers, rounded to 3 decimal places" << endl;
     file.close();
-    cout  -------------------------------------------------------------  endl;
-    file.open(num_file.txt);
+    cout << "-------------------------------------------------------------" << endl;
+    file.open("num_file.txt");
 
-    vectordouble numbers;  Create vector
-     Read and store the numbers in the vector
-    while(file  num){
+    vector<double> numbers; // Create vector
+    // Read and store the numbers in the vector
+    while(file >> num){
         numbers.push_back(num);
     }
 
-    cout  endl  b) Total numbers stored in the vector   numbers.size() = count endl;
-    cout  -------------------------------------------------------------  endl;
+    cout << endl << "b) Total numbers stored in the vector: " <<  numbers.size() /*= count*/ << endl;
+    cout << "-------------------------------------------------------------" << endl;
 
-    ofstream binaryFile(num_file.bin,iosbinary);  Create binary file
+    ofstream binaryFile("num_file.bin",ios::binary); // Create binary file
     if(!binaryFile){
-        cerr  Error creating the binary file!  endl;
+        cerr << "Error creating the binary file!" << endl;
         return 1;
     }
 
-    binaryFile.write(reinterpret_castchar(numbers.data()), numbers.size()  sizeof(double));
+    binaryFile.write(reinterpret_cast<char*>(numbers.data()), numbers.size() * sizeof(double));
 
     binaryFile.close();
 
-    cout  c) Binary file 'num_file.bin' successfully created!  endl;
+    cout << "c) Binary file 'num_file.bin' successfully created!" << endl;
 
     return 0;
 }
